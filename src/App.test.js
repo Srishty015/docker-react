40aa17ba85ac2802f514c5568e107f02df822c6b
Srishty015 <srishty.bajoriya2312@gmail.com>
2020-06-03 15:49:50 +0530

    changing test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
 import { render } from "@testing-library/react";
 import App from "./App";
 
@@ -7,9 +7,4 @@ test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
 });
